14d7de42f707702f4ac78110e1d9f206dde17809
ZwerOxotnik <zweroxotnik@gmail.com>
2020-07-30 16:48:18 +0300

    Update info

--- portable-ogg-lua-module-generator.bat
+++ portable-ogg-lua-module-generator.bat
@@ -12,7 +12,8 @@ IF EXIST %CFG_FILE% del %CFG_FILE% /f /q
 echo ### Please, do not change this file however this file don't have [programmable-speaker-instument]! More info: https://www.reddit.com/r/ZwerOxotnik/comments/dv7tpx/you_want_your_own_mod_stay_tuned/ >> %CFG_FILE%
 echo [programmable-speaker-note] >> %CFG_FILE%
 
-echo -- Please, do not change this file, except sounds_list.name! More info: https://www.reddit.com/r/ZwerOxotnik/comments/dv7tpx/you_want_your_own_mod_stay_tuned/ >> %SOUNDS_LIST_FILE%
+echo -- Please, do not change this file if you're not sure, except sounds_list.name and path! >> %SOUNDS_LIST_FILE%
+echo -- More info: https://www.reddit.com/r/ZwerOxotnik/comments/dv7tpx/you_want_your_own_mod_stay_tuned/ >> %SOUNDS_LIST_FILE%
 echo -- You need require this file to your control.lua and add https://mods.factorio.com/mod/zk-lib in your dependencies >> %SOUNDS_LIST_FILE%
 echo local sounds_list = { >> %SOUNDS_LIST_FILE%
 echo 	name = nil, --change me, if you want add these sounds to programmable speakers >> %SOUNDS_LIST_FILE%
