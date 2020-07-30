5c8735c8febc51e3e5c5a2fa7ce87796ff5f7d69
ZwerOxotnik <zweroxotnik@gmail.com>
2020-07-30 16:47:14 +0300

    Fix, add path

--- portable-ogg-lua-module-generator.bat
+++ portable-ogg-lua-module-generator.bat
@@ -16,6 +16,7 @@ echo -- Please, do not change this file, except sounds_list.name! More info: htt
 echo -- You need require this file to your control.lua and add https://mods.factorio.com/mod/zk-lib in your dependencies >> %SOUNDS_LIST_FILE%
 echo local sounds_list = { >> %SOUNDS_LIST_FILE%
 echo 	name = nil, --change me, if you want add these sounds to programmable speakers >> %SOUNDS_LIST_FILE%
+echo	path = "__your-mod-name__/this-folder-name/", -- path to this folder >> %SOUNDS_LIST_FILE%
 echo 	sounds = { >> %SOUNDS_LIST_FILE%
 for %%i in (*.ogg) do (
 	echo 		{ >> %SOUNDS_LIST_FILE%
