84870a0127fcd0bf16ba701c3d4ec562b3e34271
ZwerOxotnik <zweroxotnik@gmail.com>
2020-07-31 13:37:41 +0300

    Improve

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
-Just put and run that portable-ogg-lua-module-generator.bat from the repository in a folder with ogg files and then require from a lua file with some scripts and move the files.
+This script generates locales and lua files to handle your sound without programming!
+Tested on Windows 10 before.
 
-Launch portable-ogg-lua-module-generator.bat to check it.
+Files "141258__eternitys__interface3.ogg" and "346835__yatoimtop__pageturn.ogg" for example, so you test in this folder, probably.
 
-You can handle the new sound list via
-https://mods.factorio.com/mod/play-use-any-sound
-or https://github.com/ZwerOxotnik/zk-lib
+Instruction: copy "launch me to handle sounds.bat" file and paste it in your mod directory where you have sounds then launch "launch me to handle sounds.bat" file.
 
-Example: https://mods.factorio.com/mod/party-stuff
+If everything works the you'll similar text at the end to this:
 
-More info on https://www.reddit.com/r/ZwerOxotnik/comments/dv7tpx/you_want_your_own_mod_stay_tuned/
+>You're almost ready!
+>
+> \# You need to write 'require("__my-mod__/folder/sounds_list.lua")' in your my-mod/control.lua\
+> \# Add string "zk-lib" in dependencies of my-mod/info.json, example: '"dependencies": ["zk-lib"]'\
+> \# Put sounds_list.cfg in folder my-mod/locale/en (it'll provide readable text in the game)
+>
+> If you found a bug or you have a problem with script etc, please, let us know
+> https://github.com/ZwerOxotnik/Mod-generator
+>
+>
+> Press any key to continue ...
---
+++ launch me to handle sounds.bat
@@ -0,0 +1,61 @@
+#!/bin/bash
+@echo off
+
+set true=1==1
+set false=1==0
+SET SOUNDS_LIST_FILE=sounds_list.lua
+SET CFG_FILE=sounds_list.cfg
+
+cls
+set /p mod_name=Insert your mod name: 
+echo[
+echo you're in %CD%
+echo[
+set /p folder_path=Complete path to this folder: %mod_name%/
+choice /C YN /M "Press Y to add sounds in this folder to programmable speakers (in Factorio), N to cancel the operation."
+set state=%ERRORLEVEL%
+IF %state% EQU 1 set /p sound_group_name=Insert group name of sounds: 
+IF %state% EQU 1 (
+	IF [%sound_group_name%]==[] set /p sound_group_name=%mod_name%
+)
+IF EXIST %SOUNDS_LIST_FILE% del %SOUNDS_LIST_FILE% /f /q
+IF EXIST %CFG_FILE% del %CFG_FILE% /f /q
+
+IF %state% EQU 1 echo ### Please, do not change this file manually! >> %CFG_FILE%
+IF %state% EQU 1 echo [programmable-speaker-instument] >> %CFG_FILE%
+IF %state% EQU 1 echo %sound_group_name%=%sound_group_name% >> %CFG_FILE%
+IF %state% EQU 1 echo[ >> %CFG_FILE%
+IF %state% EQU 1 echo [programmable-speaker-note] >> %CFG_FILE%
+
+echo -- Please, do not change this file if you're not sure, except sounds_list.name and path! >> %SOUNDS_LIST_FILE%
+echo -- You need require this file to your control.lua and add https://mods.factorio.com/mod/zk-lib in your dependencies >> %SOUNDS_LIST_FILE%
+echo[ >> %SOUNDS_LIST_FILE%
+echo local sounds_list = { >> %SOUNDS_LIST_FILE%
+IF %state% EQU 1 echo 	name = %sound_group_name%, --change me, if you want to add these sounds to programmable speakers >> %SOUNDS_LIST_FILE%
+IF %state% EQU 2 echo 	name = nil --change me, if you want to add these sounds to programmable speakers >> %SOUNDS_LIST_FILE%
+echo 	path = "__%mod_name%__/%folder_path%/", -- path to this folder >> %SOUNDS_LIST_FILE%
+echo 	sounds = { >> %SOUNDS_LIST_FILE%
+for %%i in (*.ogg) do (
+	echo 		{ >> %SOUNDS_LIST_FILE%
+	echo 			name = "%%~ni", >> %SOUNDS_LIST_FILE%
+	echo 		}, >> %SOUNDS_LIST_FILE%
+	IF %state% EQU 1 echo %%~ni=%%~ni >> %CFG_FILE%
+)
+echo 	} >> %SOUNDS_LIST_FILE%
+echo } >> %SOUNDS_LIST_FILE%
+echo[ >> %SOUNDS_LIST_FILE%
+echo if puan_api then puan_api.add_sounds(sounds_list) end >> %SOUNDS_LIST_FILE%
+echo[ >> %SOUNDS_LIST_FILE%
+echo return sounds_list >> %SOUNDS_LIST_FILE%
+echo[
+cls
+echo You're almost ready!
+echo[
+echo # You need to write 'require("__%mod_name%__/%folder_path%/%SOUNDS_LIST_FILE%")' in your %mod_name%/control.lua
+echo # Add string "zk-lib" in dependencies of %mod_name%/info.json, example: '"dependencies": ["zk-lib"]'
+IF %state% EQU 1 echo # Put sounds_list.cfg in folder %mod_name%/locale/en (it'll provide readable text in the game)
+echo[
+echo If you found a bug or you have a problem with script etc, please, let us know
+echo https://github.com/ZwerOxotnik/Mod-generator
+echo[
+timeout /t -1
